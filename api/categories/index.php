2b391beb8c844286bca444f0b18bf9baff289d11
Mohammad Almohammad <almohammadmohammad116@gmail.com>
2025-03-24 00:34:42 +0300

    index.php

--- api/categories/index.php
+++ api/categories/index.php
@@ -6,16 +6,21 @@ include_once "../../models/Category.php";
 $database = new Database();
 $db = $database->getConnection();
 
-try{
+try {
     $category = new Category($db);
     $stmt = $category->read();
     $categories = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    http_response_code(200);
-    echo json_encode($categories);
-}
-catch (Exception $e) {
+
+    if (count($categories) > 0) {
+        http_response_code(200);
+        echo json_encode($categories, JSON_UNESCAPED_UNICODE);
+    } else {
+        http_response_code(404);
+        echo json_encode(["status" => "error", "message" => "لم يتم العثور على أي أصناف."], JSON_UNESCAPED_UNICODE);
+    }
+} catch (Exception $e) {
     http_response_code(500);
     echo json_encode(["status" => "error", "message" => "خطأ في الخادم: " . $e->getMessage()], JSON_UNESCAPED_UNICODE);
 }
-
 ?>
+
