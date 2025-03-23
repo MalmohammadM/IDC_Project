0faf31b370175bec7a65619e7418838c5674a601
Mohammad Almohammad <almohammadmohammad116@gmail.com>
2025-03-24 00:35:52 +0300

    index.php

--- api/users/index.php
+++ api/users/index.php
@@ -6,16 +6,19 @@ include_once "../../models/User.php";
 $database = new Database();
 $db = $database->getConnection();
 
-try{
-
+try {
     $user = new User($db);
     $stmt = $user->read();
     $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    http_response_code(200);
-    echo json_encode($users);
 
-}
-catch (Exception $e) {
+    if (count($users) > 0) {
+        http_response_code(200);
+        echo json_encode($users, JSON_UNESCAPED_UNICODE);
+    } else {
+        http_response_code(404);
+        echo json_encode(["status" => "error", "message" => "لم يتم العثور على أي مستخدمين."], JSON_UNESCAPED_UNICODE);
+    }
+} catch (Exception $e) {
     http_response_code(500);
     echo json_encode(["status" => "error", "message" => "خطأ في الخادم: " . $e->getMessage()], JSON_UNESCAPED_UNICODE);
 }
