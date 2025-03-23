132214573a497ae0da881d6bb8f0bd1f10e3b075
Mohammad Almohammad <almohammadmohammad116@gmail.com>
2025-03-24 00:33:04 +0300

    index.php

--- api/products/index.php
+++ api/products/index.php
@@ -6,15 +6,21 @@ include_once "../../models/Product.php";
 $database = new Database();
 $db = $database->getConnection();
 
-try{
+try {
     $product = new Product($db);
     $stmt = $product->read();
     $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    http_response_code(200);
-    echo json_encode($products);
-}
-catch (Exception $e) {
+
+    if (count($products) > 0) {
+        http_response_code(200);
+        echo json_encode($products, JSON_UNESCAPED_UNICODE);
+    } else {
+        http_response_code(404);
+        echo json_encode(["status" => "error", "message" => "لم يتم العثور على أي منتجات."], JSON_UNESCAPED_UNICODE);
+    }
+} catch (Exception $e) {
     http_response_code(500);
     echo json_encode(["status" => "error", "message" => "خطأ في الخادم: " . $e->getMessage()], JSON_UNESCAPED_UNICODE);
 }
 ?>
+
